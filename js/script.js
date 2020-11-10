8d9d7b88b044e382d8ecba48f67b6d6e4baffcb4
nandipatel <60127512+nandipatel@users.noreply.github.com>
2020-11-09 21:14:30 -0600

    added js for the form as validation

--- js/script.js
+++ js/script.js
@@ -13,3 +13,12 @@ function active(){
 for(i = 0; i < name.length; i++) {
 	name[i].addEventListener("click",active())
 }
+
+/* Form JS */
+function valiForm() {
+  var x = document.forms["sign_up_form"]["email_id"].value;
+  if (x == "") {
+    alert("Please enter in your email!");
+    return false;
+  }
+}
